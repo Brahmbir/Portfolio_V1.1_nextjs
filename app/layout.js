706184d4cd1da1358b5c4b39d4db83bc580a1028
brahmbir <brahmbir68@gmail.com>
2023-12-17 12:14:50 +0530

    adding openGraph

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,48 @@
-// import { Roboto_Mono } from 'next/font/google'
 import NavBar from '@/components/navBar/NavBar'
 import ChatComponent from '@/components/chatBox/ChatBox'
 import './globals.css'
 
-// const inter = Roboto_Mono({ subsets: ['latin'] })
+export const metadata = {
+  title: {
+      template: '%s | Brahmbir',
+      default: 'Brahmbir'
+  },
+  metadataBase: new URL(process.env.SITEURL),
+  creator: 'Brahmbir Singh',
+  category: 'technology',
+  icons: {
+      icon: '/logo.svg',
+  },
+  robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+          index: true,
+          follow: true,
+          archive: false,
+          noimageindex: true,
+          'max-video-preview': -1,
+          'max-image-preview': 'large',
+          'max-snippet': -1,
+      },
+  }
+}
+export const viewport = {
+  themeColor: 'black',
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  colorScheme: 'dark',
+  themeColor: 'black',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}> */}
       <body>
         <NavBar/>
         {children}
--- app/page.js
+++ app/page.js
@@ -6,31 +6,26 @@ import WorkSect from "@/components/heroPage/workSection/WorkSect"
 import { getXataClient } from "@/utils/xata";
 
 export const metadata = {
-  title: {
-      template: '%s | Brahmbir',
-      default: 'Brahmbir'
+  description: "Welcome to our Software Developer Student Site - Your Ultimate Resource for Aspiring Developers! Join a vibrant community of tech enthusiasts, where we share cutting-edge programming tutorials, industry insights, and career guidance. Enhance your coding skills with hands-on projects, and explore the latest trends in software development. Whether you're a beginner or an advanced coder, our comprehensive content caters to all levels. Start your journey towards becoming a proficient software developer today!",
+  alternates: {
+    canonical: process.env.SITEURL
   },
-  colorScheme: 'dark',
-  creator: 'Brahmbir Singh',
-  category: 'technology',
-  themeColor: 'black',
-  icons: {
-      icon: '/logo.svg',
-  },
-  robots: {
-      index: true,
-      follow: true,
-      nocache: true,
-      googleBot: {
-          index: true,
-          follow: true,
-          archive: false,
-          noimageindex: true,
-          'max-video-preview': -1,
-          'max-image-preview': 'large',
-          'max-snippet': -1,
+  openGraph: {
+    title: 'Brahmbir',
+    description: "Discover Brahmbir's Software Developer Portfolio | Unleash Innovation & Expertise. Explore web & mobile projects, ML, data analysis & more. Join a visionary in shaping the digital future.",
+    url: process.env.SITEURL,
+    siteName: 'Brahmbir',
+    images: [
+      {
+        url: `/photos/OpenGraph/home.jpg`,
+        type: "image/jpg",
+        width: 1365,
+        height: 768,
+        alt: 'Picture of website main page',
       },
-  }
+    ],
+    type: 'website',
+  },
 }
 
 const xata = getXataClient();
