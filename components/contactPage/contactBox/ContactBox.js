a69f82f307cc6164c48d2f706bf8d28d28b724d6
Brahmbir <brahmbir68@gmail.com>
2025-11-20 17:24:45 +0530

    fix font size

--- components/contactPage/contactBox/ContactBox.js
+++ components/contactPage/contactBox/ContactBox.js
@@ -12,6 +12,7 @@ const ContantBox = ({ children }) => {
         <div className={styles.fornt}>{children}</div>
       </div>
       <div className={`${styles.BG} ${styles.film}`}>
+        <span className={styles.topbar}></span>
         <svg viewBox="0 0 100 100" style={{ display: "none" }}>
           <filter id="grain">
             <feTurbulence baseFrequency="0.60,0.90" result="colorNoise" />
