a1cc8b105d8c04b0aede9a8e5c867342b707488c
Brahmbir <brahmbir68@gmail.com>
2025-11-21 00:19:37 +0530

    maybe

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {domains:["us-east-1.xata.sh","us-east-1.storage.xata.sh"]},}
+const nextConfig = {
+  images: {
+    domains: ["us-east-1.xata.sh", "us-east-1.storage.xata.sh"],
+  },
+  transpilePackages: ["three"],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "supports-color": false, // 👈 Fix for react-markdown / remark / micromark
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
